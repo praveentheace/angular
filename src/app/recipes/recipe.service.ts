2e2380623b16c55d7a697f3f5487e0463d8dd5da
Praveen <praveentheace@gmail.com>
2020-03-24 22:47:41 -0700

    refactor shopping-list components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,38 @@
 import { Recipe } from './recipe.model'
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipe: Recipe[] = [
     new Recipe('Tomato rice', 'this is a tomato rice recipe', 
-      'https://upload.wikimedia.org/wikipedia/commons/c/c1/Tomato_Rice_Recipe%2C_South_Indian_Style.jpg'),
+      'https://upload.wikimedia.org/wikipedia/commons/c/c1/Tomato_Rice_Recipe%2C_South_Indian_Style.jpg',
+      [
+          new Ingredient('Tomato', 10),
+          new Ingredient('onion', 2)
+      ]),
     new Recipe('Egg Curry', 'this is a egg curry recipe', 
-      'https://upload.wikimedia.org/wikipedia/commons/1/19/Egg_curry.jpg')
+      'https://upload.wikimedia.org/wikipedia/commons/1/19/Egg_curry.jpg',
+      [
+          new Ingredient('Egg', 4),
+          new Ingredient('Tomato', 2)
+      ])
   ];
+
+  constructor(private slService: ShoppingListService){
+
+  }
 
   getRecipes(){
       return this.recipe.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 
 }
